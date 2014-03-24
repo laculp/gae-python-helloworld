53b0ff079497af2dda3a1866efaa329f41b9bf93
Chris Smith <chrsmith+github@google.com>
2014-03-23 19:49:31 -0700

    Added app.

---
+++ helloworld.py
@@ -0,0 +1,17 @@
+"""A simple webapp2 server."""
+
+import webapp2
+
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, World!')
+        # Random message, used to sanity check the app was updated.
+        self.response.write('Apple')
+
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)

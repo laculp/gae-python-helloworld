c1c6cce4fc191281e5e9f9de1849d920b624d105
Chris Smith <chrsmith@google.com>
2014-03-24 13:41:35 -0700

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -9,7 +9,7 @@ class MainPage(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!\n')
         # Random message, used to sanity check the app was updated.
-        self.response.write('Pear')
+        self.response.write('Apple')
 
 
 application = webapp2.WSGIApplication([
